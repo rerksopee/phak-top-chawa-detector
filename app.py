a5b3d103db91632d6e06981452e68cb96372d6d9
640710144 <139632741+rerksopee@users.noreply.github.com>
2026-05-19 02:11:51 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Phak Top Chawa Detector",
+    page_icon="🌿"
+)
+
+st.title("🌿 Phak Top Chawa Detector")
+st.subheader("ระบบตรวจจับและคำนวณพื้นที่กอผักตบชวา")
